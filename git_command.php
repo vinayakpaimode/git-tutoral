7e2d080ab3921216dd8c000696317d068650265a
vinayak paimode <vinayakpaimode61382gmail.com>
2023-01-19 01:12:20 +0530

    version 2  comment

--- git_command.php
+++ git_command.php
@@ -9,3 +9,21 @@
 //for create git repository 
  git init
 
+ 
+// below command will not give you error
+  git status
+
+
+// below commnd add your file staging phase
+ git add .
+
+
+//for config name
+
+git config --global user.name "vinayak paimode"
+
+//for config email
+git config --global user.email "vinayakpaimode61382gmail.com"
+
+//for commit to git
+$ git commit -m "version 1 commnent"
